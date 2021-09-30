8bc238f12a702fa44b0189146302010fbaff4efe
erzhanto <erzhariessfeld@gmail.com>
2021-10-01 02:18:38 +0700

    Survival Shooter 3D Unity

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake()
+    {
+        //find game object dengan tag
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //get Reference component
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        //perpindahan posisi player
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination(player.position);
+        }
+        else //stop move
+        {
+            nav.enabled = false;
+        }
+    }
+}
